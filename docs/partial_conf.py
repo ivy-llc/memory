812af8c1e93e3593434cd08b14b045b4a488b9ed
Kareem Morsy <kreem.morsy@hotmail.com>
2023-08-15 19:08:17 +0300

    Change `lets-unify.ai` reference to `unify.ai`

--- docs/partial_conf.py
+++ docs/partial_conf.py
@@ -3,6 +3,6 @@ from docs.conf import html_theme_options
 
 html_theme_options["switcher"][
     "json_url"
-] = "https://lets-unify.ai/docs/versions/memory.json"
+] = "https://unify.ai/docs/versions/memory.json"
 
 repo_name = "memory"
